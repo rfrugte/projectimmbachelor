cde21b73651ecadd8556589439cc953e7f6564d6
Rogier Frugte <rpafrugte@hotmail.com>
2016-05-05 20:23:51 +0200

    dsdfdsf
    juiste files

--- refextract.py
+++ refextract.py
@@ -7,7 +7,7 @@ Created on Thu May  5 16:55:41 2016
 
 import codecs
 from bs4 import BeautifulSoup
-file = codecs.open("C:/Users/RPA/AppData/Local/Programs/Python/Python35-32/convert.htm", "r","utf-8")
+file = codecs.open("C:/Users/RPA/AppData/Local/Programs/Python/Python35-32/test.htm", "r","utf-8")
 soup = BeautifulSoup(file, 'html.parser')
 x= soup.get_text()
 x = x.lower()  
@@ -15,4 +15,4 @@ y= x.rfind('references\n')
 z = x[y:]
 r=z.splitlines()
 
-del x
+print(r)
